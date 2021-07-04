a866c47a4ec22348e133926aef2281e923d5eefa
priya shannu <shannu1219@gmail.com>
2021-07-04 18:42:05 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartProductComponent } from './cart-product/cart-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductDetailComponent,
+    CartProductComponent
   ],
   imports: [
     BrowserModule,
