295e991961cb269aafe7f84302550ca9ec0fb81c
priya shannu <shannu1219@gmail.com>
2021-07-16 00:49:18 +0530

    added crud functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CartProductComponent } from './cart-product/cart-product.component';
 
+
 const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'login', component: LoginComponent},
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import {CommonService} from '../services/common.service';
+import {Router} from '@angular/router';
 import {CartService} from '../services/cart.service';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-product-detail',
@@ -8,22 +10,71 @@ import {CartService} from '../services/cart.service';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
- 
+createProductForm: FormGroup;
+ isEdit: boolean;
 @Input("allProductList") allprdts: any;
 @Input("searchedText") searchedProduct: string='';
 @Input("sortingBy") sortByOption: string='';  
 @Output() refresh:EventEmitter<boolean> = new EventEmitter(); 
 
-constructor(public commonService: CommonService, public cart: CartService) {}
-    
+constructor(private formBuilder: FormBuilder, public commonService: CommonService, public cart: CartService, private router: Router) {}
+showDeleteDialog = false;
+selectedIndex: any;
+showCreateProduct = false;
+
 ngOnInit(){
-      console.log(this.allprdts)
       this.sortByOption = 'product_name';
-    }
-    addToCart(productId,productQty){    
-      this.cart.allItems = this.allprdts;
-      this.cart.addToCart(productId,productQty,'');
-      this.refresh.emit(true);
+
+       this.createProductForm = this.formBuilder.group({
+      p_id : [''],
+      product_name : [null, Validators.required],
+      product_weight : [null],
+      product_price : [null]
+    });
+
     }
 
+    deleteProdConfirm(i) {      
+      this.selectedIndex = i;
+      this.showDeleteDialog = true;
+    }   
+
+    deleteSelectedRow(){
+      this.allprdts.splice(this.selectedIndex, 1);
+    }
+
+    navigateToCreate() {
+      this.isEdit = false;
+      this.showCreateProduct = true;
+    }
+
+  createProd() {
+    if(this.createProductForm.valid) {
+    this.allprdts.unshift(this.createProductForm.value)
+    } 
+    this.createProductForm.reset();
+    this.showCreateProduct = false;
+
+  }
+
+  editProd(prod, index) {
+    this.isEdit = true;
+    this.showCreateProduct = true;
+    this.selectedIndex = index;
+    for(let i=0; i < this.allprdts.length;i++) {
+      if(this.allprdts[i].p_id == prod.p_id) {
+        this.createProductForm.patchValue(prod);
+      }
+    }
+  }
+
+  updateProductDetail(prod) {
+    for(let i=0; i < this.allprdts.length;i++) {
+      if(i == this.selectedIndex) {
+          this.allprdts[i] = prod;
+      }
+    }
+    this.showCreateProduct = false;
+    this.createProductForm.reset();
+  }
 }
